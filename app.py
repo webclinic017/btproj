f21c431ecf2c0eb8cfb26dcbc63fc66057aad0d1
Shen, Fei <feishen@ebay.com>
2022-06-20 19:42:53 +0800

    data 20220620

--- app.py
+++ app.py
@@ -18,51 +18,87 @@ from strategies.strategynorthsma import StrategyNorthWithSMA
 
 app = Flask(__name__)
 
+strategies = [
+    {
+        "label": "Strategy4 for HS300ETF/CYB50ETF/ZZ500ETF mode 2",
+        "class": Strategy4,
+        "stocks": [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF],
+        "data_start": 60,
+        "args": {"mode": 2, "rsi": "((30, 5), (25, 5), (24, 5))"}
+    },
+    {
+        "label": "Strategy4 for HS300ETF/CYB50ETF/ZZ500ETF mode 1",
+        "class": Strategy4,
+        "stocks": [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF],
+        "data_start": 60,
+        "args": {"mode": 1}
+    },
+    {
+        "label": "StrategyNorth for CYB50ETF",
+        "class": StrategyNorth,
+        "stocks": [Stock.CYB50ETF],
+        "data_start": 0,
+        "args": {}
+    },
+    {
+        "label": "StrategyNorth for A50ETF",
+        "class": StrategyNorth,
+        "stocks": [Stock.A50ETF],
+        "data_start": 0,
+        "args": {}
+    },
+    {
+        "label": "StrategyNorthWithSMA for CYB50ETF",
+        "class": StrategyNorthWithSMA,
+        "stocks": [Stock.CYB50ETF],
+        "data_start": 60,
+        "args": {}
+    },
+    {
+        "label": "StrategyNorthWithSMA for A50ETF",
+        "class": StrategyNorthWithSMA,
+        "stocks": [Stock.A50ETF],
+        "data_start": 60,
+        "args": {}
+    },
+    {
+        "label": "StrategyNorthWithSMA for A50ETF New Args",
+        "class": StrategyNorthWithSMA,
+        "stocks": [Stock.A50ETF],
+        "data_start": 60,
+        "args": {"periodbull": 250, "highpercentbull": 0.8, "lowpercentbull": 0.4, "maxdrawbackbull": 0.05,
+                 "periodbear": 120, "highpercentbear": 0.9, "lowpercentbear": 0.2, "maxdrawbackbear": 0.1,
+                 "smaperiod": 10}
+    },
+    {
+        "label": "StrategySMA for CYB50ETF",
+        "class": StrategySMA,
+        "stocks": [Stock.CYB50ETF],
+        "data_start": 60,
+        "args": {"mode": 1}
+    }
+]
+
 
 @app.route("/")
 def home():
-    return """<html>
+    strategy_contents = ""
+    for i in range(len(strategies)):
+        strategy = strategies[i]
+        strategy_contents += """<h1>
+    %s
+    <a href="log/%d">Log</a> <a href="plot/%d">Plot</a>
+    </h1>
+    <br/><br/>""" % (strategy["label"], i, i)
+
+    content = """<html>
     <head>
     <title>Daily Strategy</title>
     </head>
     <body>
     <a href="daily"><h1>Daily Strategy</h1></a>
     <br/><br/>
-    <h1>
-    Strategy4 for HS300ETF/CYB50ETF/ZZ500ETF mode 2
-    <a href="log/1">Log</a> <a href="plot/1">Plot</a>
-    </h1>
-    <br/><br/>
-    <h1>
-    Strategy4 for HS300ETF/CYB50ETF/ZZ500ETF mode 1
-    <a href="log/4">Log</a> <a href="plot/4">Plot</a>
-    </h1>
-    <br/><br/>
-    <h1>
-    StrategyNorth for CYB50ETF
-    <a href="log/2">Log</a> <a href="plot/2">Plot</a>
-    </h1>
-    <br/><br/>
-    <h1>
-    StrategyNorth for A50ETF
-    <a href="log/3">Log</a> <a href="plot/3">Plot</a>
-    </h1>
-    <br/><br/>
-    <h1>
-    StrategyNorthWithSMA for CYB50ETF
-    <a href="log/5">Log</a> <a href="plot/5">Plot</a>
-    </h1>
-    <br/><br/>
-    <h1>
-    StrategyNorthWithSMA for A50ETF
-    <a href="log/6">Log</a> <a href="plot/6">Plot</a>
-    </h1>
-    <br/><br/>
-    <h1>
-    StrategySMA for CYB50ETF
-    <a href="log/7">Log</a> <a href="plot/7">Plot</a>
-    </h1>
-    <br/><br/>
+    """ + strategy_contents + """
     <a href="load/sina"><h1>Load Latest Data Sina</h1></a>
     <br/><br/>
     <a href="load/tx"><h1>Load Latest Data Tencent</h1></a>
@@ -70,6 +106,7 @@ def home():
     <a href="datalist"><h1>Show Data List</h1></a>
     </body>
     </html>"""
+    return content
 
 
 @app.route("/daily", defaults={'start_date': '2021-08-25', 'start_trade_date': None})
@@ -77,19 +114,11 @@ def home():
 @app.route('/daily/<string:start_date>/<string:start_trade_date>')
 def daily_strategy(start_date, start_trade_date):
     logs = []
-    logs = logs + run(Strategy4, [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF], start=start_date, data_start=60, starttradedt=start_trade_date, mode=2, rsi=((30, 5), (25, 5), (24, 5)))
-    logs.append('')
-    logs = logs + run(Strategy4, [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF], start=start_date, data_start=60, starttradedt=start_trade_date, mode=1)
-    logs.append('')
-    logs = logs + run(StrategyNorth, [Stock.CYB50ETF], start=start_date)
-    logs.append('')
-    logs = logs + run(StrategyNorth, [Stock.A50ETF], start=start_date)
-    logs.append('')
-    logs = logs + run(StrategyNorthWithSMA, [Stock.CYB50ETF], data_start=60, start=start_date)
-    logs.append('')
-    logs = logs + run(StrategyNorthWithSMA, [Stock.A50ETF], data_start=60, start=start_date, periodbull=250, highpercentbull=0.8, lowpercentbull=0.4, maxdrawbackbull=0.05, periodbear=120, highpercentbear=0.9, lowpercentbear=0.2, maxdrawbackbear=0.1, smaperiod=10)
-    logs.append('')
-    logs = logs + run(StrategySMA, [Stock.CYB50ETF], data_start=60, start=start_date, mode=1)
+    for strategy in strategies:
+        logs.append(strategy["label"])
+        logs = logs + run(strategy["class"], strategy["stocks"], start=start_date, data_start=strategy["data_start"],
+                      starttradedt=start_trade_date, **strategy["args"])
+        logs.append('')
     return '<a href="/">Back</a><br/><br/>' + "<br/>".join(logs)
 
 
@@ -97,21 +126,10 @@ def daily_strategy(start_date, start_trade_date):
 @app.route('/log/<int:id>/<string:start_date>', defaults={'start_trade_date': None})
 @app.route('/log/<int:id>/<string:start_date>/<string:start_trade_date>')
 def daily_strategy_logs(id, start_date, start_trade_date):
-    logs = []
-    if id == 1:
-        logs = run(Strategy4, [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF], start=start_date, data_start=60, starttradedt=start_trade_date, printLog=True, mode=2, rsi=((30, 5), (25, 5), (24, 5)))
-    elif id == 2:
-        logs = run(StrategyNorth, [Stock.CYB50ETF], start=start_date, starttradedt=start_trade_date, printLog=True)
-    elif id == 3:
-        logs = run(StrategyNorth, [Stock.A50ETF], start=start_date, starttradedt=start_trade_date, printLog=True)
-    if id == 4:
-        logs = run(Strategy4, [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF], start=start_date, data_start=60, starttradedt=start_trade_date, printLog=True, mode=1)
-    elif id == 5:
-        logs = run(StrategyNorthWithSMA, [Stock.CYB50ETF], start=start_date, starttradedt=start_trade_date, data_start=60, printLog=True)
-    elif id == 6:
-        logs = run(StrategyNorthWithSMA, [Stock.A50ETF], start=start_date, starttradedt=start_trade_date, data_start=60, printLog=True, periodbull=250, highpercentbull=0.8, lowpercentbull=0.4, maxdrawbackbull=0.05, periodbear=120, highpercentbear=0.9, lowpercentbear=0.2, maxdrawbackbear=0.1, smaperiod=10)
-    elif id == 7:
-        logs = run(StrategySMA, [Stock.CYB50ETF], start=start_date, starttradedt=start_trade_date, data_start=60, printLog=True, mode=1)
+    strategy = strategies[id]
+    logs = [strategy["label"]]
+    logs = logs + run(strategy["class"], strategy["stocks"], start=start_date, data_start=strategy["data_start"],
+               starttradedt=start_trade_date, printLog=True, **strategy["args"])
     return '<a href="/">Back</a><br/><br/>' + "<br/>".join(logs)
 
 
@@ -119,21 +137,9 @@ def daily_strategy_logs(id, start_date, start_trade_date):
 @app.route('/plot/<int:id>/<string:start_date>', defaults={'start_trade_date': None})
 @app.route('/plot/<int:id>/<string:start_date>/<string:start_trade_date>')
 def daily_strategy_plot(id, start_date, start_trade_date):
-    html = 'Invalid strategy'
-    if id == 1:
-        html = run_plot(Strategy4, [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF], start=start_date, data_start=60, starttradedt=start_trade_date, printLog=False, mode=2, rsi="((30, 5), (25, 5), (24, 5))")
-    elif id == 2:
-        html = run_plot(StrategyNorth, [Stock.CYB50ETF], start=start_date, starttradedt=start_trade_date, printLog=False)
-    elif id == 3:
-        html = run_plot(StrategyNorth, [Stock.A50ETF], start=start_date, starttradedt=start_trade_date, printLog=False)
-    if id == 4:
-        html = run_plot(Strategy4, [Stock.HS300ETF, Stock.CYB50ETF, Stock.ZZ500ETF], start=start_date, data_start=60, starttradedt=start_trade_date, printLog=False, mode=1)
-    elif id == 5:
-        html = run_plot(StrategyNorthWithSMA, [Stock.CYB50ETF], start=start_date, starttradedt=start_trade_date, data_start=60, printLog=False)
-    elif id == 6:
-        html = run_plot(StrategyNorthWithSMA, [Stock.A50ETF], start=start_date, starttradedt=start_trade_date, data_start=60, printLog=False, periodbull=250, highpercentbull=0.8, lowpercentbull=0.4, maxdrawbackbull=0.05, periodbear=120, highpercentbear=0.9, lowpercentbear=0.2, maxdrawbackbear=0.1, smaperiod=10)
-    elif id == 7:
-        html = run_plot(StrategySMA, [Stock.CYB50ETF], start=start_date, starttradedt=start_trade_date, data_start=60, printLog=False, mode=1)
+    strategy = strategies[id]
+    html = run_plot(strategy["class"], strategy["stocks"], start=start_date, data_start=strategy["data_start"],
+                    starttradedt=start_trade_date, printLog=False, **strategy["args"])
     return html
 
 
@@ -162,7 +168,8 @@ def datalist():
     def generate():
         yield '<a href="/">Back</a><br/><br/>'
         for stock in stocks.Stock:
-            yield '<h1><a href="data/%s">%s</a> <a href="dataplot/%s">Plot</a></h1>' % (stock.code, stock.cnname, stock.code)
+            yield '<h1><a href="data/%s">%s</a> <a href="dataplot/%s">Plot</a></h1>' % (
+                stock.code, stock.cnname, stock.code)
         yield '<a href="data/%s"><h1>%s</h1></a>' % ('north_all', 'North All')
         yield '<a href="data/%s"><h1>%s</h1></a>' % ('north_sh', 'North Shanghai')
         yield '<a href="data/%s"><h1>%s</h1></a>' % ('north_sz', 'North Shenzhen')
@@ -174,6 +181,7 @@ def datalist():
 @app.route('/data/<stock_code>/<int:rows>')
 def data(stock_code, rows):
     stock = get_stock(stock_code)
+
     def generate():
         lines = pathlib.Path(get_datafile_name(stock_code)).read_text().split("\n")
         lines = [lines[0]] + lines[-rows:][::-1]
@@ -196,6 +204,7 @@ def data(stock_code, rows):
                 yield "</tr>"
         yield "</table>"
         yield "</body></html>"
+
     return app.response_class(stream_with_context(generate()))
 
 
