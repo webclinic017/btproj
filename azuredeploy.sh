428083b03f9befcfe7d5f0e351223412ab3fe601
Shen, Fei <feishen@ebay.com>
2022-06-28 22:03:33 +0800

    data 20220628

--- azuredeploy.sh
+++ azuredeploy.sh
@@ -1,1 +1,1 @@
-az webapp up --sku F1 --name feitrade --runtime "PYTHON|3.8"
+az webapp up --sku F1 --name feitrade --runtime "PYTHON|3.8" -g emmelle_0_rg_Linux_southcentralus -p emmelle_0_asp_Linux_southcentralus_0 -l southcentralus
---
+++ azuredeploy2.sh
@@ -0,0 +1,1 @@
+az webapp up --sku F1 --name feitrade2 --runtime "PYTHON|3.8" -g trade2 -p ASP-trade2-8532 -l eastasia
